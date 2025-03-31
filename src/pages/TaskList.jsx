352dd561740f24963b8d1a98ac676179a378e794
Arela-Cristina <instagrandine@gmail.com>
2025-03-31 20:06:05 +0200

    refactor list structure and style

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,17 +1,34 @@
 import style from './TaskList.module.css'
+import { useContext } from 'react'
+import taskContext from '../global-context/TaskGlobalContext'
 
 
 
 export default function TaskList() {
 
+    const task = useContext(taskContext)
+
     return (
 
         <section className={style.customTaskList}>
-            <div className={style.titleTaskList}>Tasks</div>
-            <ul>
-                <li>da fare</li>
-            </ul>
-        </section>
+    <div className={style.headerRow}>
+        <div className={style.column}>Nome</div>
+        <div className={style.column}>Stato</div>
+        <div className={style.column}>Data di Creazione</div>
+    </div>
+    <ul>
+        <li className={style.row}>
+            <div className={style.column}>Una tarea con un nombre super largo que podría desbordar</div>
+            <div className={style.column}>Fatto</div>
+            <div className={style.column}>13/11/1995</div>
+        </li>
+        <li className={style.row}>
+            <div className={style.column}>Otra tarea</div>
+            <div className={style.column}>In corso</div>
+            <div className={style.column}>20/05/2024</div>
+        </li>
+    </ul>
+</section>
 
     )
 
