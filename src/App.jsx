39dec7d235a3c47be3adc851b37b522eb16ef88d
Arela-Cristina <instagrandine@gmail.com>
2025-03-31 18:00:44 +0200

    feat: Implement global context and fetch for tasks
    - Created a global context (`taskContext`) to share the tasks state across the application.
    - Implemented `TaskGlobalProvider` to wrap the app and provide the context to components.
    - Made a `fetch` request to the API to retrieve tasks and store them in the global state using `setTask`.
    - Called `fetchTasks` inside `useEffect` to load tasks when the component mounts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,44 @@
 import { BrowserRouter, Routes, Route } from "react-router";
+import { useState, useEffect } from "react";
+import taskContext from './global-context/TaskGlobalContext'
 import Index from "./Index";
 import AddTask from "./pages/AddTask";
 import TaskList from './pages/TaskList'
 
+const BASE_API = import.meta.env.VITE_API_URL;
 
 function App() {
 
 
+  const [task, setTask] = useState([])
+
+  async function fetchTasks() {
+    try {
+      const response = await fetch(`${BASE_API}/tasks`);
+      const data = await response.json();
+      setTask(data)
+      console.log('data:', data)
+    } catch (error) {
+      console.error(`error fetching taks.`, error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTasks();
+  }, [])
+
   return (
 
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />}>
-          <Route path="addTask" element={<AddTask />} />
-          <Route path="taskList" element={<TaskList />} />
-        </Route>
-      </Routes>
-    </BrowserRouter >
-
+    <taskContext.Provider value={{ task, setTask }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />}>
+            <Route path="addTask" element={<AddTask />} />
+            <Route path="taskList" element={<TaskList />} />
+          </Route>
+        </Routes>
+      </BrowserRouter >
+    </taskContext.Provider>
 
   )
 }
