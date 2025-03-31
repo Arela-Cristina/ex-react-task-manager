8e00a1e6859c5aace886579f17de824d941e0d7d
Arela-Cristina <instagrandine@gmail.com>
2025-03-31 20:12:56 +0200

    add style

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -11,24 +11,19 @@ export default function TaskList() {
     return (
 
         <section className={style.customTaskList}>
-    <div className={style.headerRow}>
-        <div className={style.column}>Nome</div>
-        <div className={style.column}>Stato</div>
-        <div className={style.column}>Data di Creazione</div>
-    </div>
-    <ul>
-        <li className={style.row}>
-            <div className={style.column}>Una tarea con un nombre super largo que podría desbordar</div>
-            <div className={style.column}>Fatto</div>
-            <div className={style.column}>13/11/1995</div>
-        </li>
-        <li className={style.row}>
-            <div className={style.column}>Otra tarea</div>
-            <div className={style.column}>In corso</div>
-            <div className={style.column}>20/05/2024</div>
-        </li>
-    </ul>
-</section>
+            <div className={style.headerRow}>
+                <div className={style.column}>Nome</div>
+                <div className={style.column}>Stato</div>
+                <div className={style.column}>Data di Creazione</div>
+            </div>
+            <ul>
+                <li className={style.row}>
+                    <div className={style.column}>Una tarea con un nombre super largo que podría desbordar</div>
+                    <div className={style.column}>Fatto</div>
+                    <div className={style.column}>13/11/1995</div>
+                </li>
+            </ul>
+        </section>
 
     )
 
