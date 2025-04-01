d649fe2aedb644d5dff3766c23b56d952bff222e
Arela-Cristina <instagrandine@gmail.com>
2025-04-01 17:48:33 +0200

    fix: remove onChange from uncontrolled fields and add conditional rendering for error messages

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,61 +1,91 @@
 import { useState, useRef } from "react"
+import style from './AddTask.module.css'
 
 export default function AddTask() {
 
-
-
     const [name, setName] = useState('')
-    // console.log('name', name)
-
-    const descriptionRef = useRef();
-
+    console.log(name)
+    const [controlName, setControlName] = useState(true)
+    const descriptionRef = useRef(null);
     const stateRef = useRef(null)
-    console.log('stato', stateRef)
+
+
+    function handleNameChange(e) {
+        let newName = e.target.value;
+
+        if (!newName.trim()) {
+            setControlName(false);
+            console.log('Campo vuoto');
+        }
+
+        const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", "\\", ",", ".", "<", ">", "?", "/", "`", "~"];
+
+        if (symbols.some(char => newName.includes(char))) {
+            setControlName(false);
+            console.log('Non accetta Simboli');
+            return;
+        }
+
+        setControlName(true);
+        setName(newName);
+    }
 
     return (
+        <>
+            <h2>Planner</h2>
 
-        <form action="">
+            <form onSubmit={() => { }}>
 
-            <div>
-                <div>Nome del task </div>
-                <input
-                    name='name'
-                    type="text"
-                    value={name}
-                    onChange={(e) => { setName(e.target.value) }}
-                />
-            </div>
+                <div>
+                    <div>Nome del task </div>
+                    <input
+                        name='name'
+                        type="text"
+                        value={name}
+                        onChange={handleNameChange}
+                    />
+                    {(!controlName) ? (
+                        <p className={style.textError}>Non può contenere simboli speciali.</p>
+                    )
+                        : (
+                            <p className={style.textCorrect}>Nome del task puo includere numeri</p>
 
-            <div>
-                <div>Descrizione</div>
-                <input
-                    name='descrizione'
-                    type="text"
-                    ref={descriptionRef}
-                    onChange={(e) => { e.target.descriptionRef }}
-                />
-            </div>
-
-            <div>
-                <div>Stato</div>
-                <select
-                    name="state"
-                    id="state"
-                    ref={stateRef}
-                    onChange={() => { console.log(stateRef.current.value) }}>
-
-                    <option value='null'>Selezziona stato</option>
-                    <option value="toDo">To Do</option>
-                    <option value="doing">Doing</option>
-                    <option value="done">Done</option>
-                </select>
-            </div>
+                        )}
+                </div>
 
 
 
-        </form>
+                <div>
+                    <div>Descrizione</div>
+                    <input
+                        name='descrizione'
+                        type="text"
+                        ref={descriptionRef}
+
+                    />
+                </div>
+
+                <div>
+                    <div>Stato</div>
+                    <select
+                        name="state"
+                        id="state"
+                        ref={stateRef}>
 
 
+                        <option value='null'>Selezziona stato</option>
+                        <option value="toDo">To Do</option>
+                        <option value="doing">Doing</option>
+                        <option value="done">Done</option>
+                    </select>
+                </div>
+
+                <button type="submit"> Aggiungi</button>
+
+
+            </form >
+
+        </>
 
     )
 
