87a97daa3193854e9f9bd52004618d59af2dfe15
Arela-Cristina <instagrandine@gmail.com>
2025-04-01 16:02:10 +0200

    feat: import useState to control input name value

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,9 +1,41 @@
-
+import { useState } from "react"
 
 export default function AddTask() {
 
+    const [name, setName] = useState('')
+    console.log('name', name)
+
     return (
-        <div>Add Task</div>
+
+        <form action="">
+
+            <div>
+                <div>Nome del task </div>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => { setName(e.target.value) }}
+                />
+            </div>
+
+            <div>
+                <div>Descrizione</div>
+                <input type="text" />
+            </div>
+
+            <div>
+                <div>Stato</div>
+                <select name="" id="">
+                    <option value={null}>Selezziona stato</option>
+                    <option value="toDo">To Do</option>
+                    <option value="doing">Doing</option>
+                    <option value="done">Done</option>
+                </select>
+            </div>
+
+
+
+        </form>
 
 
 
