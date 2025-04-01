c8c15e1527946f5111737321c272d7a1a027a7a0
Arela-Cristina <instagrandine@gmail.com>
2025-04-01 12:38:45 +0200

    feat: Create a function to assign color dynamically and implement React.memo to optimize the TaskRow component.

--- src/component/TaskRow.jsx
+++ src/component/TaskRow.jsx
@@ -1,13 +1,35 @@
 import style from './TaskRow.module.css';
+import React from 'react'
 
-export default function ({ task }) {
+
+
+function TaskRow({ task }) {
 
     return (
 
         <li className={style.row} key={task.id}>
             <div className={style.column}> {task.title} </div>
-            <div className={style.column}> {task.status} </div>
+            <div className={`${style.column}, ${getStatusClass(task.status)}`}> {task.status} </div>
             <div className={style.column}> {task.createdAt} </div>
         </li>
     )
+}
+
+
+export default React.memo(TaskRow)
+
+
+
+// funzione per assegnare i colori dinamicamente
+function getStatusClass(status) {
+    switch (status) {
+        case "To do":
+            return style.toDo;
+        case "Doing":
+            return style.doing;
+        case "Done":
+            return style.done;
+        default:
+            return '';
+    }
 }
