22d29b4cedf09f5b2b47cd4a18d180be5e3fca29
Arela-Cristina <instagrandine@gmail.com>
2025-04-02 16:44:42 +0200

    feat: Create Modal component and implement React Portal to render it above the document body

---
+++ src/component/Modal.jsx
@@ -0,0 +1,25 @@
+import { createPortal } from 'react-dom'
+import style from './Modal.module.css'
+
+export default function Modal({
+    title = 'title',
+    content = 'contenuto',
+    show = false,
+    onClose = () => { },
+    onConfirm = () => { },
+    confirmText = 'Conferma' }) {
+
+    if (!show) return null;
+
+    return createPortal(
+        <div className={style.modalOverlay}>
+            <div className={style.modalContent}>
+                <h2>{title}</h2>
+                <p>{content}</p>
+                <button onClick={onClose}>Chiudi</button>
+                <button onClick={onConfirm}>{confirmText}</button>
+            </div>
+        </div>,
+        document.body
+    )
+}
