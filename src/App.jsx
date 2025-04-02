9faeb9a4502f9806735bafc3a50f0d20687c45fb
Arela-Cristina <instagrandine@gmail.com>
2025-04-02 13:51:51 +0200

    add new route for Task Detail

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,25 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import taskContext from './global-context/TaskGlobalContext'
 import Index from "./DefaultLayout";
 import AddTask from "./pages/AddTask";
-import TaskList from './pages/TaskList'
+import TaskList from './pages/TaskList';
+import TaskDetail from './pages/TaskDetail'
 import useTasks from "./custom Hooks/useTasks";
 
 
 function App() {
 
 
-  const  taskData = useTasks()
+  const taskData = useTasks()
 
   return (
-    
+
     <taskContext.Provider value={{ ...taskData }}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />}>
             <Route path="addTask" element={<AddTask />} />
             <Route path="taskList" element={<TaskList />} />
+            <Route path="/task/:id" element={<TaskDetail />} />
           </Route>
         </Routes>
       </BrowserRouter >
