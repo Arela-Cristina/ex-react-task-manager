53447d9c34596002fea3a7f64a555c410d2da3e9
Arela-Cristina <instagrandine@gmail.com>
2025-04-03 18:29:21 +0200

    create Menu Nav module css to add style

--- src/component/MenuNav.jsx
+++ src/component/MenuNav.jsx
@@ -1,20 +1,22 @@
-import { NavLink } from "react-router"
-
+import { NavLink } from "react-router";
+import style from './MenuNav.module.css';
 
 export default function Index() {
-
     return (
-        <>
+        <section className={style.kawaiiContainer}>
+            <NavLink
+                to="/addTask"
+                className={({ isActive }) => `${style.kawaiiButton} ${isActive ? "active" : ""}`}
+            >
+                 Add Task 
+            </NavLink>
 
-            <NavLink to='/addTask' className={({ isActive, isPending }) =>
-                isPending ? "pending" : isActive ? "active" : ""
-            }> Add Task </NavLink>
-
-            <NavLink to='/taskList'> Task List </NavLink>
-
-        </>
-
-
-    )
-
+            <NavLink
+                to="/taskList"
+                className={({ isActive }) => `${style.kawaiiButton} ${isActive ? "active" : ""}`}
+            >
+                 Task List 
+            </NavLink>
+        </section>
+    );
 }
