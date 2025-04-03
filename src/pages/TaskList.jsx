ea653a7734c2a06dce4095138aec0b1f61ad68a5
Arela-Cristina <instagrandine@gmail.com>
2025-04-03 14:20:10 +0200

    feat: create a Search Query state and filter tasks while keeping order with sort

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -10,6 +10,10 @@ export default function TaskList() {
     const { tasks } = useContext(taskContext)
     console.log('ecco mi', tasks)
 
+    // filtro di ricerca
+    const [searchQuery, setSearchQuery] = useState('')
+    console.log('query', searchQuery)
+
     const [sortBy, setSortBy] = useState('createdAt')
 
     const [sortOrder, setSortOrder] = useState(1)
@@ -26,45 +30,52 @@ export default function TaskList() {
 
     const sortArrowIcon = sortOrder === 1 ? '🠉' : '🠋';
 
-
     // memo per memorizzare il compito di una funzione, ameno che le loro prop cambiano, per assicurare la fluidezza del nostro programmino
     const sortTasks = useMemo(() => {
 
 
-        return [...tasks].sort((a, b) => {
-            let order;
+        return [...tasks]
+            .filter(task => task.title.toLowerCase().includes(searchQuery.toLowerCase()))
+            .sort((a, b) => {
+                let order;
 
-            switch (sortBy) {
-                case "title":
-                    let titleA = a.title.toLowerCase();
-                    let titleB = b.title.toLowerCase();
-                    order = titleA.localeCompare(titleB);
-                    return order * sortOrder;
+                switch (sortBy) {
+                    case "title":
+                        let titleA = a.title.toLowerCase();
+                        let titleB = b.title.toLowerCase();
+                        order = titleA.localeCompare(titleB);
+                        return order * sortOrder;
 
-                case "status":
-                    const statusValues = ["To do", "Doing", "Done"];
-                    const statoA = statusValues.indexOf(a.status);
-                    const statoB = statusValues.indexOf(b.status);
-                    order = statoA - statoB;
-                    return order * sortOrder;
+                    case "status":
+                        const statusValues = ["To do", "Doing", "Done"];
+                        const statoA = statusValues.indexOf(a.status);
+                        const statoB = statusValues.indexOf(b.status);
+                        order = statoA - statoB;
+                        return order * sortOrder;
 
-                case "createdAt":
-                    const dateA = new Date(a.createdAt).getTime();
-                    const dateB = new Date(b.createdAt).getTime();
-                    order = dateA - dateB;
-                    return order * sortOrder;
+                    case "createdAt":
+                        const dateA = new Date(a.createdAt).getTime();
+                        const dateB = new Date(b.createdAt).getTime();
+                        order = dateA - dateB;
+                        return order * sortOrder;
 
-                default:
-                    return 0;
-            }
-        });
+                    default:
+                        return 0;
+                }
+            });
 
-    }, [tasks, sortBy, sortOrder])
+    }, [tasks, sortBy, sortOrder, searchQuery])
 
 
 
     return (
         <>
+            <input
+                type="search"
+                value={searchQuery}
+                onChange={(e) => { setSearchQuery(e.target.value) }}
+            />
+
             {!tasks || tasks.length === 0 ? (<p>Cercando tasks ...</p>)
 
 
